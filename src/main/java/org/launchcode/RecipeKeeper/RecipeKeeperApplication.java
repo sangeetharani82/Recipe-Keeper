c4f54192774a15fe84eec5adad268152a2ef7cf4
Sangeetha Rani <sangeetha.sabarinath@gmail.com>
2019-01-23 12:48:07 -0600

    initial commit

---
+++ src/main/java/org/launchcode/RecipeKeeper/RecipeKeeperApplication.java
@@ -0,0 +1,14 @@
+package org.launchcode.RecipeKeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeKeeperApplication.class, args);
+	}
+
+}
+
